97638503a7113215141289537b70199d52e921c0
Lucas Gabriel Silva Xavier <lsilva-x@c1r4p2.42sp.org.br>
2025-02-07 15:58:18 -0300

    feature: Input validation

--- header.h
+++ header.h
@@ -6,7 +6,7 @@
 /*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 18:50:28 by lsilva-x          #+#    #+#             */
-/*   Updated: 2025/02/06 19:09:22 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/02/07 15:00:13 by lsilva-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "libft/includes/libft.h"
+
+void	input_check(int argc, char **argv);
+void	exit_program(void);
 
 # endif
